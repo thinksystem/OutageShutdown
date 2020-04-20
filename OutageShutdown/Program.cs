b2b19fa20f8b4a1dbd56d7c8482bad0cb1c87a3d
ryckN hax0r <43122627+thinksystem@users.noreply.github.com>
2020-04-20 01:49:44 +0100

    +

--- OutageShutdown/Program.cs
+++ OutageShutdown/Program.cs
@@ -2,16 +2,36 @@
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Diagnostics;
 
 namespace OutageShutdown
 {
     class Program
     {
+        static string targetip = "192.168.1.30";
+        static int howmuchping = 5;
+        static int waitperping = 15000;
+
         static void Main()
         {
-            Console.WriteLine("write ip you want to ping");
-            string input = Console.ReadLine();
-            Console.WriteLine(SendPing(input));
+            int failedpings = 0;
+
+            for (int i = 0; i < howmuchping; i++)
+            {
+                bool result = SendPing(targetip);
+                if (result) Environment.Exit(0);
+                if (!result) failedpings++;
+                Console.WriteLine("PING " + failedpings + " " + result);
+                System.Threading.Thread.Sleep(waitperping);
+            }
+            
+            if (failedpings >= howmuchping)
+            {
+                Console.WriteLine("SHUTTING DOWN");
+                //Process.Start("shutdown", "/p");
+            }
+
+            Console.WriteLine("DONE " + failedpings);
             Console.ReadLine();
         }
         static bool SendPing(string ip)
@@ -25,12 +45,10 @@ namespace OutageShutdown
             PingReply reply = ping.Send(ip, timeout, buffer, options);
             if (reply.Status == IPStatus.Success)
             {
-                Console.WriteLine("Got reply from " + ip + ".");
                 return true;
             }
             else
             {
-                Console.WriteLine("Host unreachable.");
                 return false;
             }
         }
